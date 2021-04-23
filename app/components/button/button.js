6df70f385aa2b2bbcdeea09ad622fcb7b6dba15e
simei-yeh <simei.yeh@gmail.com>
2021-04-23 10:17:14 -0700

    created button, form, input components

---
+++ app/components/button/button.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Button = ({ show, onClose, children, title }) => {
+  return (
+    <button>
+      Close
+    </button>
+  )
+};
+
+export default Button;
