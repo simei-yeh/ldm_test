1c2f27fdc9397caf85154e9043a8a0d69c3a4d3a
simei-yeh <simei.yeh@gmail.com>
2021-04-22 17:45:42 -0700

    created components with basic modal

---
+++ app/components/modal/modals.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = ({ show, onClose, children, title }) => {
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
+  const handleCloseClick = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
+  const modalContent = show ? (
+  <>
+  </>
+  ) : null;
+
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById('modal-container')
+    );
+  } else {
+    return null;
+  }
+};
+
+
+export default Modal;
