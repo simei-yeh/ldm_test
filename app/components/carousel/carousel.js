57a5a31d4d3f9af8d4dca326e9c6bf2f09e28223
simei-yeh <simei.yeh@gmail.com>
2021-04-24 15:41:57 -0700

    reflected form in carousel

--- app/components/carousel/carousel.js
+++ app/components/carousel/carousel.js
@@ -1,26 +1,77 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Button from '../button/button';
-import Form from '../form/form';
+import Form from '../form/form.tsx';
+import Step from '../step/step.tsx';
+import Input from '../input/input'
+import Options from '../input/options'
+import Select from '../input/select'
+import Textarea from '../input/textarea'
 import styles from './carousel.module.css';
 
-const Carousel = ({ children, formPages }) => {
-  const keys = Object.keys(formPages);
+const Carousel = ({ inputs }) => {
+  const pages = 4;
+  const [step, setStep] = useState(1);
+  const [values, setValues] = useState({name: '', date: '', mgmtfees: '', newInvestors: '',
+    fundType: '', returns: '', confirmation: ''});
+  const [showForwardButton, setShowForwardButton] = useState(true);
+  const [showBackButton, setShowBackButton] = useState(false);
+  const handleInputChange = e => {
+    const { name, value } = e.target
+    console.log('this is working', name, value)
+    setValues({ ...values, [name]: value })
+  }
+
+  useEffect(() => {
+    if (step === 1) setShowBackButton(false);
+    else if (step === pages) setShowForwardButton(false);
+    else if (!showForwardButton) setShowForwardButton(true);
+    else if (!showBackButton) setShowBackButton(true);
+  }, [step])
 
   return (
     <div className={styles['carousel-container']}>
       <div>
         <Button
+          show={showForwardButton}
           id="forward"
           text="Continue"
-          callback={() => {  }}
+          callback={() => { setStep(step + 1) }}
         />
-        <Button id="backward"
+        <Button
+          show={showBackButton}
+          id="backward"
           text="Go Back"
-          callback={() => {  }}
+          callback={() => { setStep(step - 1) }}
         />
       </div>
+
       <div id="track" className={styles['carousel-track']} >
-        {keys.map((k, i) => <Form key={k} formList={formPages[k]} />)}
+        <Form>
+          <Step message="Enter fund name and date">
+            <Input type="text" label="Fund Name" callback={handleInputChange} />
+            <Input type="date" label="Date" name="date" callback={handleInputChange} />
+          </Step>
+          <Step message="Enter ad description and management fees">
+            <Textarea />
+            <Select type="select" name="Management Fees" id="mgmtfees"
+              callback={handleInputChange}>
+              {["", "10%", "15%", "20%"].map((fee, i) => <Options value={fee} />)}
+            </Select>
+          </Step>
+          <Step message="Choose fund types">
+            <span>{`Open to New Investors?`}</span>
+            {['Open', 'Closed'].map((option, i) => <Input type="radio" value={option}
+              name="newInvestors" />)}
+            <span>{`Fund type`}</span>
+            {['Hedge Fund', 'Venture Capital', 'Private Equity'].map((option, i) =>
+              <Input type="checkbox" value={option} name="fundType" />)}
+          </Step>
+          <Step message="Confirm yearly returns and submit">
+            <Input type="range" min="0%" max="100%" step="5%" label="Yearly Returns"
+              name="returns" />
+            <Input type="image" src="/submit.png" />
+          </Step>
+        </Form>
       </div>
     </div>
   )
---
+++ app/components/form/form.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './form.module.css';
+
+const Form = ({ children }) => {
+  return (
+    <form id="form" className={styles['form-content']}>
+      {children}
+    </form>
+  )
+}
+
+export default Form;
