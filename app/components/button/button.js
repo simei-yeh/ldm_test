a85f56771840f29e34df26fbf1b907cb460a3e69
simei-yeh <simei.yeh@gmail.com>
2021-04-24 10:25:07 -0700

    added conditional formatting to button

--- app/components/button/button.js
+++ app/components/button/button.js
@@ -2,12 +2,18 @@ import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './button.module.css';
 
-const Button = ({ onClose, children, text, id, callback }) => {
+const Button = ({ show, onClose, children, text, id, callback }) => {
   return (
-    <span onClick={callback}
-      className={`${styles[id]} ${styles['button']}`}>
-      {text}
-    </span>
+    <div>
+      {
+        show
+          ? <span onClick={callback}
+              className={`${styles[id]} ${styles['button']}`}>
+              {text}
+            </span >
+          : null
+      }
+    </div>
   )
 };
 
