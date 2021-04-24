17d34f059a73b55b2f167b8b058e3cb9f74a0415
simei-yeh <simei.yeh@gmail.com>
2021-04-23 19:25:51 -0700

    added button properties in destructure

--- app/components/button/button.js
+++ app/components/button/button.js
@@ -5,7 +5,7 @@ import styles from './button.module.css';
 const Button = ({ show, onClose, children, title, name, id, callback }) => {
   return (
     <span onClick={callback}
-      className={styles[id]}>
+      className={`${styles[id]} ${styles['button']}`}>
       {name}
     </span>
   )
