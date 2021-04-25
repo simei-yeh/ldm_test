29e308ddb1ee05d613becd5f4e58f788a779650d
simei-yeh <simei.yeh@gmail.com>
2021-04-24 21:58:00 -0700

    modified inputs to use new labels

--- components/input/input.js
+++ components/input/input.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './input.module.css';
 
-const Input = ({ label, type, name, value, src, callback }) => {
+const Input = ({ label, type, name, value, src, callback, callbackSubmit }) => {
   return (
     <div className={`${styles['input-container']}`}>
-      <label htmlFor={type} className={styles['label']}>{label ? label : value}</label>
+      <label htmlFor={type} className={styles['label']}>{label ? label : name}</label>
       <input type={type} className={`${styles['input']} ${styles[type]}`}
         value={value} name={name} src={src} onChange={callback}></input>
     </div>
