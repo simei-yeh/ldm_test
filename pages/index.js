c16f6d705b65916bf5e8e69e7b24d399ba3bae7d
simei-yeh <simei.yeh@gmail.com>
2021-04-24 21:19:45 -0700

    added submission handling to save state in index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Button from '../components/button/button'
 import Modals from '../components/modal/modals'
 import React, { useEffect, useRef, useState } from 'react';
 
@@ -11,13 +12,25 @@ export async function getStaticProps() {
 
 export default function Home({ inputs }) {
   const [showModal, setShowModal] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [submittedValues, setSubmittedValues] = useState([]);
+
+  const isSubmitted = (values) => {
+    event.preventDefault();
+    console.log(values)
+    setSubmittedValues([...submittedValues, values]);
+    setSubmitted(true);
+    setShowModal(false);
+    console.log(submittedValues, submitted);
+  }
 
   return (
     <div>
-        <div onClick={() => setShowModal(true)}>Enter a new fund</div>
+        <Button callback={() => setShowModal(true)} text="Enter a new fund" show={true} id="entry" />
         <Modals
           onClose={() => setShowModal(false)}
           show={showModal}
+          submission={isSubmitted}
           inputs={inputs}
         >
         </Modals>
