69d5381e935e3b9504ac594f2e28716c02ca313a
simei-yeh <simei.yeh@gmail.com>
2021-04-24 21:20:15 -0700

    minor change in select to show label

--- components/input/select.js
+++ components/input/select.js
@@ -4,7 +4,7 @@ import styles from './input.module.css';
 const Select = ({ type, name, id, children, callback, label }) => {
   return (
     <>
-      <label htmlFor={type} className={styles['label']}>{label}</label>
+      <label htmlFor={type} className={styles['label']}>{name}</label>
       <select name={name} id={id} required onChange={callback}>
         {children}
       </select>
