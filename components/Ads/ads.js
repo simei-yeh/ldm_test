60c18759ee824055d764da5de994c624a099b9b8
simei-yeh <simei.yeh@gmail.com>
2021-04-24 22:30:17 -0700

    added ads display and some styling

--- components/Ads/ads.js
+++ components/Ads/ads.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import styles from './ads.module.css';
 
 const Ads = ({ createdAds }) => {
+  const keys = createdAds.length >= 1 ? Object.keys(createdAds[0]) : [];
   return (
     <div className={styles["ad-container"]}>
-      {createdAds.map((ad, i) => <div>{ad["Fund Name"]}</div>)}
+      {createdAds.map((ad, i) =>
+        <div key={i} className={styles["ad"]}>
+          {keys.map((k, i) => <span className={styles["ad-content"]}>
+            {`${k}: `}{ad[k]}</span>)}
+        </div>)}
     </div>
-  )
+    )
 };
 
 export default Ads;
